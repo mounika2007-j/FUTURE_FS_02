84a3830e8e13993239b6024756c15f299310f5da
Jenjuluru Mounika <jmounika118@email.com>
2026-03-21 08:36:40 +0530

    Made some changes

--- app.py
+++ app.py
@@ -201,6 +201,6 @@ def home():
 
 # ── ENTRY POINT ─────────────────────────────────────────────────────────────
 if __name__ == "__main__":
-    init_db()  # ensure DB exists
+    init_db()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
